5b1ba1949e0bd8bd927c2824885eb1d897a484d5
1ne8ight <tanoheliezerbonny@gmail.com>
2025-10-01 19:32:54 +0000

    Ajout soustraction

--- application/routes.py
+++ application/routes.py
@@ -8,10 +8,16 @@ def addition(a: int, b: int) -> int:
     # return a - b
     return a + b
 
+def soustraction(a: int, b: int) -> int:
+    """Retourne la difference entre deux nombres."""
+    return a - b
+    # return a + b
+
 
 @app.route("/")
 def index():
     result = addition(2, 5)
+    result_sous = soustraction(2, 5)
     # app.logger.info(f"Résultat du calcul : {result}")
 
     return render_template("index.html", result=result)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,21 @@
 import pytest
+from application.routes import addition, soustraction, app
 
-from application.routes import addition, app
 
-
+# Tests unitaires pour les fonctions
 def test_addition():
     assert addition(2, 5) == 7
     assert addition(-1, 1) == 0
     assert addition(0, 0) == 0
 
 
+def test_soustraction():
+    assert soustraction(5, 2) == 3
+    assert soustraction(2, 5) == -3
+    assert soustraction(0, 0) == 0
+
+
+# Fixture pour le client Flask
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
@@ -16,7 +23,9 @@ def client():
         yield client
 
 
+# Test de la route index
 def test_index_route(client):
     response = client.get("/")
     assert response.status_code == 200
+    # Vérifie que le résultat de addition est bien dans le rendu HTML
     assert b"7" in response.data
